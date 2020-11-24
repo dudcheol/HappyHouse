d111cab574e13ef58e17b826d645c370ea117f22
박영철 <>
2020-11-24 16:14:02 +0900

    인프라 지수 계산 + ui

--- happyhouse/src/components/map/detail/Infra.vue
+++ happyhouse/src/components/map/detail/Infra.vue
@@ -1,12 +1,64 @@
 <template>
   <b-container>
-    <h1>this is infra.vue</h1>
-    <h6>{{ getConveniences }}</h6>
+    <b-row class="px-3 pb-3" align-h="end">
+      <b-button @click="allOpen" size="sm" variant="" pill>{{
+        openStr
+      }}</b-button>
+    </b-row>
+    <b-card
+      no-body
+      class="mb-3"
+      v-for="(items, index) in getAllCategory"
+      :key="index"
+    >
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block v-b-toggle="'collapse-' + index" variant="light"
+          ><b-row class="px-2" align-v="center">
+            <b-col class="text-left p-0">
+              <h6 class="mb-0 pl-2 font-weight-bolder text-left">
+                <b-icon :icon="icon[index]"></b-icon>
+                {{ category[index] }}
+              </h6>
+            </b-col>
+            <b-col class="text-right p-0">
+              <b-form-rating
+                v-model="items.idx"
+                readonly
+                no-border
+                variant="warning"
+                inline
+                class="p-0 bg-transparent"
+              ></b-form-rating>
+            </b-col> </b-row
+        ></b-button>
+      </b-card-header>
+      <b-collapse
+        :id="'collapse-' + index"
+        :visible="visible[index]"
+        role="tabpanel"
+      >
+        <b-card-body>
+          <b-row class="px-2">
+            <h6
+              class="p-0 my-1"
+              style="color:dimgrey"
+              v-for="(item, index) in items"
+              :key="index"
+            >
+              <b-badge variant="light">{{ item.distance }}m</b-badge>거리에
+              <b-badge variant="light">{{ item.place_name }}</b-badge
+              >이(가) 있어요
+            </h6>
+          </b-row>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
   </b-container>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
+
 export default {
   props: {
     lat: String,
@@ -14,7 +66,7 @@ export default {
   },
   watch: {
     lat: function() {
-      this.CONVENIENCE({
+      this.ALLCATEGORY({
         radius: this.radius,
         x: this.lng,
         y: this.lat,
@@ -23,20 +75,51 @@ export default {
   },
   data() {
     return {
-      radius: 100,
+      radius: 1000,
+      visible: [false, false, false, false, false, false],
+      category: ['편의', '교육', '교통', '의료', '카페', '문화'],
+      allVisible: false,
+      openStr: '모두 펼치기',
+      icon: [
+        'cart',
+        'book',
+        'stoplights',
+        'clipboard-plus',
+        'cup',
+        'controller',
+      ],
     };
   },
-  //   beforeUpdate() {
-  //     console.log('beforeUpdate:' + this.lat + ',' + this.lng);
-  //   },
-  //   updated() {
-  //     console.log('updated:' + this.lat + ',' + this.lng);
-  //   },
   computed: {
-    ...mapGetters(['getConveniences']),
+    ...mapGetters([
+      'getConveniences',
+      'getEducations',
+      'getTraffics',
+      'getMedicals',
+      'getCafes',
+      'getCultures',
+      'getAllCategory',
+    ]),
   },
   methods: {
-    ...mapActions(['CONVENIENCE']),
+    ...mapActions([
+      'CONVENIENCE',
+      'EDUCATION',
+      'TRAFFIC',
+      'MEDICAL',
+      'CAFE',
+      'CULTURE',
+      'ALLCATEGORY',
+    ]),
+    allOpen() {
+      if (this.allVisible)
+        this.visible = [false, false, false, false, false, false];
+      else this.visible = [true, true, true, true, true, true];
+
+      this.openStr = this.allVisible ? '모두 펼치기' : '모두 접기';
+
+      this.allVisible = !this.allVisible;
+    },
   },
 };
 </script>
--- happyhouse/src/store/modules/infrastore.js
+++ happyhouse/src/store/modules/infrastore.js
@@ -13,6 +13,7 @@ const mapstore = {
     medicals: [],
     cafes: [],
     cultures: [],
+    allCategory: [],
   },
   getters: {
     getConveniences(state) {
@@ -32,6 +33,16 @@ const mapstore = {
     },
     getCultures(state) {
       return state.cultures;
+    },
+    getAllCategory(state) {
+      return [
+        state.conveniences,
+        state.educations,
+        state.traffics,
+        state.medicals,
+        state.cafes,
+        state.cultures,
+      ];
     },
   },
   mutations: {
@@ -55,31 +66,168 @@ const mapstore = {
     },
   },
   actions: {
+    ALLCATEGORY(store, payload) {
+      store.dispatch('CONVENIENCE', payload);
+      store.dispatch('EDUCATION', payload);
+      store.dispatch('TRAFFIC', payload);
+      store.dispatch('MEDICAL', payload);
+      store.dispatch('CAFE', payload);
+      store.dispatch('CULTURE', payload);
+    },
     CONVENIENCE(store, payload) {
       return axios
         .all([getConv(payload), getMart(payload)])
         .then(
-          axios.spread((conv, mart) => {
-            console.log(
-              '현재 [좌표]에서 검색된 [편의점]의 수 : ' +
-                conv.data.documents.length
+          axios.spread((res1, res2) => {
+            console.log('[편의점]의 수 : ' + res1.data.documents.length);
+            console.log('[대형마트]의 수 : ' + res2.data.documents.length);
+            store.commit(
+              'CONVENIENCE',
+              Object.assign(res1.data.documents, res2.data.documents, {
+                idx: calidx('CONVENIENCE', {
+                  a: res1.data.documents.length,
+                  adist: getDistance(
+                    res1.data.documents.length,
+                    res1.data.documents[0]
+                  ),
+                  b: res2.data.documents.length,
+                  bdist: getDistance(
+                    res2.data.documents.length,
+                    res2.data.documents[0]
+                  ),
+                }),
+              })
             );
-            console.log(
-              '현재 [좌표]에서 검색된 [대형마트]의 수 : ' +
-                mart.data.documents.length
-            );
-            store.commit('CONVENIENCE', conv.data);
           })
         )
         .catch((err) => {
           console.error(err);
         });
     },
-    // EDUCATION(store, payload) {},
-    // TRAFFIC(store, payload) {},
-    // MEDICAL(store, payload) {},
-    // CAFE(store, payload) {},
-    // CULTURE(store, payload) {},
+    EDUCATION(store, payload) {
+      return axios
+        .all([getSchool(payload), getPSchool(payload)])
+        .then(
+          axios.spread((res1, res2) => {
+            console.log('[학교]의 수 : ' + res1.data.documents.length);
+            console.log('[학원]의 수 : ' + res2.data.documents.length);
+
+            store.commit(
+              'EDUCATION',
+              Object.assign(res1.data.documents, res2.data.documents, {
+                idx: calidx('EDUCATION', {
+                  a: res1.data.documents.length,
+                  adist: getDistance(
+                    res1.data.documents.length,
+                    res1.data.documents[0]
+                  ),
+                  b: res2.data.documents.length,
+                  bdist: getDistance(
+                    res2.data.documents.length,
+                    res2.data.documents[0]
+                  ),
+                }),
+              })
+            );
+          })
+        )
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+    TRAFFIC(store, payload) {
+      return getMetro(payload)
+        .then((res) => {
+          console.log('[지하철]의 수 : ' + res.data.documents.length);
+          store.commit(
+            'TRAFFIC',
+            Object.assign(res.data.documents, {
+              idx: calidx('TRAFFIC', {
+                a: res.data.documents.length,
+                adist: getDistance(
+                  res.data.documents.length,
+                  res.data.documents[0]
+                ),
+              }),
+            })
+          );
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    MEDICAL(store, payload) {
+      return axios
+        .all([getHospital(payload), getDrugstore(payload)])
+        .then(
+          axios.spread((res1, res2) => {
+            console.log('[병원]의 수 : ' + res1.data.documents.length);
+            console.log('[약국]의 수 : ' + res2.data.documents.length);
+            store.commit(
+              'MEDICAL',
+              Object.assign(res1.data.documents, res2.data.documents, {
+                idx: calidx('MEDICAL', {
+                  a: res1.data.documents.length,
+                  adist: getDistance(
+                    res1.data.documents.length,
+                    res1.data.documents[0]
+                  ),
+                  b: res2.data.documents.length,
+                  bdist: getDistance(
+                    res2.data.documents.length,
+                    res2.data.documents[0]
+                  ),
+                }),
+              })
+            );
+          })
+        )
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    CAFE(store, payload) {
+      return getCafe(payload)
+        .then((res) => {
+          console.log('[카페]의 수 : ' + res.data.documents.length);
+          store.commit(
+            'CAFE',
+            Object.assign(res.data.documents, {
+              idx: calidx('CAFE', {
+                a: res.data.documents.length,
+                adist: getDistance(
+                  res.data.documents.length,
+                  res.data.documents[0]
+                ),
+              }),
+            })
+          );
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    CULTURE(store, payload) {
+      return getCulture(payload)
+        .then((res) => {
+          console.log('[문화시설]의 수 : ' + res.data.documents.length);
+          store.commit(
+            'CULTURE',
+            Object.assign(res.data.documents, {
+              idx: calidx('CULTURE', {
+                a: res.data.documents.length,
+                adist: getDistance(
+                  res.data.documents.length,
+                  res.data.documents[0]
+                ),
+              }),
+            })
+          );
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
   },
   modules: {},
 };
@@ -92,6 +240,8 @@ function getConv(payload) {
       x: payload.x,
       y: payload.y,
       radius: payload.radius,
+      size: 5,
+      sort: 'distance',
     },
   });
 }
@@ -103,8 +253,163 @@ function getMart(payload) {
       x: payload.x,
       y: payload.y,
       radius: payload.radius,
+      size: 5,
+      sort: 'distance',
     },
   });
+}
+function getSchool(payload) {
+  return axios.get(API_URL, {
+    headers: { Authorization: `KakaoAK ${API_KEY}` },
+    params: {
+      category_group_code: 'SC4',
+      x: payload.x,
+      y: payload.y,
+      radius: payload.radius,
+      size: 5,
+      sort: 'distance',
+    },
+  });
+}
+function getPSchool(payload) {
+  return axios.get(API_URL, {
+    headers: { Authorization: `KakaoAK ${API_KEY}` },
+    params: {
+      category_group_code: 'AC5',
+      x: payload.x,
+      y: payload.y,
+      radius: payload.radius,
+      size: 5,
+      sort: 'distance',
+    },
+  });
+}
+function getMetro(payload) {
+  return axios.get(API_URL, {
+    headers: { Authorization: `KakaoAK ${API_KEY}` },
+    params: {
+      category_group_code: 'SW8',
+      x: payload.x,
+      y: payload.y,
+      radius: payload.radius,
+      size: 5,
+      sort: 'distance',
+    },
+  });
+}
+function getCulture(payload) {
+  return axios.get(API_URL, {
+    headers: { Authorization: `KakaoAK ${API_KEY}` },
+    params: {
+      category_group_code: 'CT1',
+      x: payload.x,
+      y: payload.y,
+      radius: payload.radius,
+      size: 5,
+      sort: 'distance',
+    },
+  });
+}
+function getCafe(payload) {
+  return axios.get(API_URL, {
+    headers: { Authorization: `KakaoAK ${API_KEY}` },
+    params: {
+      category_group_code: 'CE7',
+      x: payload.x,
+      y: payload.y,
+      radius: payload.radius,
+      size: 5,
+      sort: 'distance',
+    },
+  });
+}
+function getHospital(payload) {
+  return axios.get(API_URL, {
+    headers: { Authorization: `KakaoAK ${API_KEY}` },
+    params: {
+      category_group_code: 'HP8',
+      x: payload.x,
+      y: payload.y,
+      radius: payload.radius,
+      size: 5,
+      sort: 'distance',
+    },
+  });
+}
+function getDrugstore(payload) {
+  return axios.get(API_URL, {
+    headers: { Authorization: `KakaoAK ${API_KEY}` },
+    params: {
+      category_group_code: 'PM9',
+      x: payload.x,
+      y: payload.y,
+      radius: payload.radius,
+      size: 5,
+      sort: 'distance',
+    },
+  });
+}
+
+function getDistance(cnt, doc) {
+  if (cnt == 0) return 1000;
+  else return doc.distance;
+}
+
+function calidx(category, info) {
+  let cal = 0.0;
+  switch (category) {
+    case 'CONVENIENCE':
+      if (info.a >= 1) {
+        cal += 2;
+        if (info.adist <= 500) cal += 0.5;
+        if (info.a >= 3) cal += 0.5;
+      }
+      if (info.b >= 1) {
+        cal += 2;
+      }
+      return cal;
+    case 'EDUCATION':
+      if (info.a >= 1) {
+        cal += 3;
+        if (info.adist <= 500) cal += 1;
+        else if (info.adist <= 700) cal += 0.5;
+      }
+      if (info.b >= 1) {
+        cal += 1;
+      }
+      return cal;
+    case 'TRAFFIC':
+      if (info.a >= 1) {
+        cal += 3;
+        if (info.adist <= 500) cal += 2;
+        else if (info.adist <= 700) cal += 1;
+        else if (info.adist <= 900) cal += 0.5;
+      } else cal += 1.5;
+      return cal;
+    case 'MEDICAL':
+      if (info.a >= 1) {
+        cal += 2;
+      } else if (info.a >= 2) {
+        cal += 3;
+      }
+      if (info.b >= 1) {
+        cal += 2;
+      }
+      return cal;
+    case 'CAFE':
+      if (info.a >= 1) {
+        cal += 2;
+        if (info.a >= 3) cal += 1;
+        if (info.adist <= 500) cal += 2;
+        else if (info.adist <= 700) cal += 1;
+        else if (info.adist <= 900) cal += 0.5;
+      }
+      return cal;
+    case 'CULTURE':
+      if (info.a >= 1) cal += 2;
+      cal += info.a;
+      return cal >= 5 ? 5 : cal;
+  }
 }
 
 export default mapstore;
